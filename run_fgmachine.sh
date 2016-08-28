d2f2217a169de4d7ceea7246c5d9398dffa897ab
exuser <exuser@vmh2>
2016-08-28 13:19:18 -0400

    fgmachine does not need cuda bindings by default (it can run cuda-enabled applications via docker)

--- run_fgmachine.sh
+++ run_fgmachine.sh
@@ -10,9 +10,10 @@ docker run -d --name fgmachine \
     -h $HOSTNAME \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --net=host \
-    `curl -s localhost:3476/docker/cli` \
     -e FGLAB_URL=http://127.0.0.1:5080 \
     -e FGMACHINE_URL=http://$HOSTNAME:$PORT \
     -v $CURDIR/projects.json:/root/FGMachine/projects.json \
     -p 5081:$PORT \
     kaixhin/fgmachine
+
+#    `curl -s localhost:3476/docker/cli`
