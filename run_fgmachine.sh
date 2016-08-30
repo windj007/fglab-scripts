c73b2d811b272d744a7bb168c22e811fc0c1b0f1
Roman <windj007@gmail.com>
2016-08-30 03:56:45 -0400

    persist specs.json

--- run_fgmachine.sh
+++ run_fgmachine.sh
@@ -4,7 +4,8 @@ HOSTNAME=$(hostname)
 PORT=5081
 CURDIR="$(pwd)/$(dirname $0)"
 
-[ ! -f $CURDIR/projects.json ] && touch projects.json
+[ ! -f $CURDIR/projects.json ] && touch $CURDIR/projects.json
+[ ! -f $CURDIR/specs.json ] && touch $CURDIR/specs.json
 [ ! -d $CURDIR/results ] && mkdir results
 
 FGLAB_URL="http://$HOSTNAME:5080"
@@ -16,6 +17,7 @@ docker run -d --name fgmachine \
     -e FGLAB_URL=$FGLAB_URL \
     -e FGMACHINE_URL=http://$HOSTNAME:$PORT \
     -v $CURDIR/projects.json:/root/FGMachine/projects.json \
+    -v $CURDIR/specs.json:/root/FGMachine/specs.json \
     -v $CURDIR/results:/root/FGMachine/results \
     -p 5081:$PORT \
     kaixhin/fgmachine
